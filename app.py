aefe79d3334c90511083d8524e58dc72a23f3e7d
Paul Bartsch <pdbartsch@gmail.com>
2022-04-19 15:17:27 -0700

    todo app progress

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
 # flask run
 
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "{} is the title and {} is the description".format(
+            self.title, self.description
+        )
+
+
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    if request.method == "POST":  # IF is a post request then ...
+        title = request.form["title"]
+        description = request.form["description"]
+        todo = Todo(title=title, description=description)
+        db.session.add(todo)  # for adding values in the database
+        db.session.commit()  # save the above additions to the database
+    alltodos = Todo.query.all()  # if the request is not a post then ...
+    return render_template("index.html", todos=alltodos)
+
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    deletetodo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(deletetodo)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/updatetodo/<int:sno>", methods=["GET", "POST"])
+def updatetodo(sno):
+
+    if request.method == "POST":
+        updatetodo = Todo.query.filter_by(sno=sno).first()
+        title = request.form["title"]
+        description = request.form["description"]
+        todo = Todo(title=title, description=description)
+        updatetodo.title = title
+        updatetodo.description = description
+        db.session.commit()
+        return redirect("/")
+    updatetodo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", updatetodo=updatetodo)
+
+
+# not for production:
+if __name__ == "__main__":
+    app.run(debug=True)
