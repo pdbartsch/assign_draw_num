a284e8db4af22e75abc88bb4e40069f0f195f173
Paul Bartsch <pdbartsch@gmail.com>
2022-05-09 09:24:58 -0700

    database inserts

---
+++ sql statements/projects.sql
@@ -0,0 +1,5 @@
+-- SQLite
+INSERT INTO "drawfile" ("locnum", "drawnum", "contractnum", "projectnum", "projectmngr", "mainconsult", "title", "comments", "date") VALUES 
+
+(3,101,,"CR 121",,,"Site Restoration For Hoff Heights",,date("1949-06-03")),
+(4,106,,,,"Walter Wordeman And Welton Becket","Ucla Campus Atlas",,date("1949-01-28"));
