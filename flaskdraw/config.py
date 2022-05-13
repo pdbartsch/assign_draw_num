18cbbf347f20dc80d525074849e048ea1e6a9ed7
Paul Bartsch <pdbartsch@gmail.com>
2022-05-13 14:21:38 -0700

    trying to get cloud sql working

--- flaskdraw/config.py
+++ flaskdraw/config.py
@@ -1,22 +1,27 @@
 import os
-import urllib.parse 
-
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-use_local_database = True
+use_local_database = False
 
 class Config:
     if use_local_database:
         SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database.db")
     else:
-        POSTGRES_URL = os.environ.get("PUBLIC_IP_ADDRESS")
-        POSTGRES_USER = os.environ.get("POSTGRES_USER")
-        POSTGRES_PW = os.environ.get("POSTGRES_PW")
-        POSTGRES_DB = os.environ.get("POSTGRES_DB")
+        DB_USER=os.environ.get("DB_USER")
+        DB_PW=os.environ.get("DB_PW")
+        DB_URL=os.environ.get("DB_URL")
+        DB_DB=os.environ.get("DB_DB")   
+        DB_PROJECT_ID=os.environ.get("DB_PROJECT_ID") 
+        DB_INSTANCE_NAME=os.environ.get("DB_INSTANCE_NAME")
         
-        DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-        SQLALCHEMY_DATABASE_URI=DB_URL
+        DB_URL = 'postgresql+psycopg2://root:'+DB_PW+'@'+DB_URL+'/'+DB_DB+'?host=/cloudsql/'+DB_PROJECT_ID+':'+DB_INSTANCE_NAME
+        # DB_URL = "postgresql+psycopg2://{user}:{pw}@{url}/{db}".format(user=DB_USER,pw=DB_PW,url=DB_URL,db=DB_DB)
+        # DB_URL=postgres://user:password@/dbname?host=/path/to/db
+
+        SQLALCHEMY_DATABASE_URI = DB_URL
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get("TRACK_MODS")
+
+
 
